95a1d6ee757e673f4371cd9ad3a52ea3b1256838
Yash Shah <yash2304shah@gmail.com>
2025-10-06 17:51:35 +0530

    Making it Deplaoy Ready

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# main.py
-
 import uvicorn
 
 if __name__ == "__main__":
-    print("Starting FastAPI server on http://127.0.0.1:8000")
-    # reload=True is great for development, it automatically restarts the server on code changes
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    print("🚀 Starting FastAPI server...")
+    uvicorn.run("app:app", host="0.0.0.0", port=10000)
